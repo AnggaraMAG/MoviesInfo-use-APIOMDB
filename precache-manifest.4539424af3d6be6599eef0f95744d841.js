e35330549f4c25e889ce362c312447a699cc1d29
Anggara1234 <anggaramag@gmail.com>
2020-04-12 17:02:47 +0700

    Updates

---
+++ precache-manifest.4539424af3d6be6599eef0f95744d841.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ce74ad104cbfc42970f4ac46aea1a3",
+    "url": "/MoviesInfo-use-APIOMDB/index.html"
+  },
+  {
+    "revision": "9408355b5f1f773e1d4a",
+    "url": "/MoviesInfo-use-APIOMDB/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "691294f41c56b29c8ed2",
+    "url": "/MoviesInfo-use-APIOMDB/static/css/main.c9d691c3.chunk.css"
+  },
+  {
+    "revision": "9408355b5f1f773e1d4a",
+    "url": "/MoviesInfo-use-APIOMDB/static/js/2.e4f1a893.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MoviesInfo-use-APIOMDB/static/js/2.e4f1a893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "691294f41c56b29c8ed2",
+    "url": "/MoviesInfo-use-APIOMDB/static/js/main.553bfc73.chunk.js"
+  },
+  {
+    "revision": "e2b4e474a561dda109a6",
+    "url": "/MoviesInfo-use-APIOMDB/static/js/runtime-main.1e527e5b.js"
+  },
+  {
+    "revision": "c38d59b013ef43746bc1033e3340cb56",
+    "url": "/MoviesInfo-use-APIOMDB/static/media/spinner.c38d59b0.gif"
+  }
+]);
